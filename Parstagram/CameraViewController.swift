3328b5c9a93de345dc5d0f013ffc27cb814d8b4d
edanyachdav <edan.yachdav@gmail.com>
2019-04-13 21:03:05 -0700

    Parstagram part 2 finished

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -59,7 +59,7 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
         let size = CGSize(width: 300, height: 300)
-        let scaledImaged = image.af_imageScaled(to: size)
+        let scaledImaged = image.af_imageAspectScaled(toFill: size)
         
         imageView.image = scaledImaged
         
---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by edan yachdav on 4/13/19.
+//  Copyright © 2019 edan yachdav. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
